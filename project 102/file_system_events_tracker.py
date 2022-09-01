e527e6218b8735142e5c7c53e2c81aec849aed43
Mxnya2005 <71551721+Mxnya2005@users.noreply.github.com>
2022-09-01 17:48:05 +0530

    Add files via upload

---
+++ project 102/file_system_events_tracker.py
@@ -0,0 +1,11 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/manya/Downloads/"
